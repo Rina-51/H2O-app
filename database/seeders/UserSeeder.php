9f3c7200c7beb273fb30e601c31263ff3d84a96a
Rina-51 <rina05peach01@gmail.com>
2024-09-23 20:02:57 +0900

    migration途中，userのseed

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use DateTime;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->count(10)->create();
+        
+        DB::table('users')->insert([
+            'name' => "test",
+            'email' => "test@gmail.com",
+            'email_verified_at' => now(),
+            'password' => Hash::make('testtest'),
+            'remember_token' => Str::random(10),
+         ]);
+    }
+}
