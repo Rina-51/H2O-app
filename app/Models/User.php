8f1f28f3e8a1fc2328490d3586135aafa203104f
Rina-51 <rina05peach01@gmail.com>
2024-10-09 21:06:31 +0900

    リレーションの記述

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,16 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function posts()   
+    {
+        return $this->hasMany(Post::class);  
+    }
+    
+    public function groups()   
+    {
+        return $this->belongsToMany(Group::class);  
+    }
+
     /**
      * The attributes that are mass assignable.
      *
